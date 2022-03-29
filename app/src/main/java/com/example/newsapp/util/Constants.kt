38f6499d10baf5cba8073af1a783f8eac700426a
susheet <susheet13@gmail.com>
2022-03-29 13:24:14 +0530

    second commit

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "657e05953e66427696240ba127515a82"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
