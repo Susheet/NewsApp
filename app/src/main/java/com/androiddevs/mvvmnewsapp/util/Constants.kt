b12bc272b25bdcb46d4444064bc935d944cfe094
susheet <susheet13@gmail.com>
2022-04-03 23:48:52 +0530

    Final commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "657e05953e66427696240ba127515a82"
+        const val BASE_URL = "https://newsapi.org"
+        const  val SEARCH_NEWS_TIME_DELAY = 500L
+    }
+}
